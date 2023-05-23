0ad47cb36a088aaba8032c3bb88ba079d0435374
jose.regalado <jose.regalado@irapuato.gob.mx>
2023-05-23 15:52:25 -0600

    Avance curso

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import Home from '@/components/Home.vue'
+import Splashscreen from '@/components/Splashscreen.vue'
+import { defineAsyncComponent } from 'vue';
+
+const HomeC = defineAsyncComponent(() => {
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(Home);
+    }, 3000);
+  });
+});
+
+</script>
+
+<template>
+    <Suspense>
+        <template #default>
+            <Home />
+        </template>
+        <template #fallback>
+            <Splashscreen />
+        </template>
+    </Suspense>
+</template>
+
+<style scoped>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #070707;
+}
+</style>
