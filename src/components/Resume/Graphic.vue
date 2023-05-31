02f69447b52eb7b126ef37990b2ca56f609e2a1e
Jose Juan Regalado <jregaladomoncada@gmail.com>
2023-05-31 07:58:01 -0600

    Avance

--- src/components/Resume/Graphic.vue
+++ src/components/Resume/Graphic.vue
@@ -1,14 +1,14 @@
 import { computed } from 'vue';
 <template>
     <div>
-        <svg viewBox="0 0 300 200">
+        <svg viewBox="0 0 300 200" @touchstart="tap" @touchmove="tap" @touchend="unTap" >
             <line 
              stroke="#c4c4c4"
              stroke-with="2"
              x1="0"
-             y1="100"
+             :y1="zero"
              x2="300"
-             y2="100"
+             :y2="zero"
             />
             <polyline 
                 fill="none"
@@ -16,20 +16,20 @@ import { computed } from 'vue';
                 stroke-width="2"
                 :points="points"/>
                 <line 
+                v-show="showPointer"
                 stroke="#04b500"
                 stroke-with="2"
-                x1="200"
+                :x1="pointer"
                 y1="0"
-                x2="200"
+                :x2="pointer"
                 y2="200"/>
 
         </svg>
         <p>Últimos 30 días</p>
-        {{ points  }}
     </div>
 </template>
 <script setup>
-import { computed, defineProps, toRefs } from 'vue';
+import { computed, toRefs, ref } from 'vue';
 
 const props = defineProps({
     amounts: {
@@ -41,24 +41,46 @@ const props = defineProps({
 const { amounts } = toRefs(props);
 
 
-const amountToPixels = () => {
+const amountToPixels = (amount) => {
     const min = Math.min(...amounts.value);
     const max = Math.max(...amounts.value);
 
-    return `${min}, ${max}`;
+    const amountAbs = amount + Math.abs(min);
+    const minmax = Math.abs(max) + Math.abs(min);
+
+
+    return 200 -((amountAbs * 100) / minmax) *2 ;
 }
+
+const zero = computed(() => {
+    return amountToPixels(0);
+});
 
 
 
 const points = computed(() => {
     const total = amounts.value.length;
-    return Array(total).fill(100).reduce((points, amount, i) => {
+    return amounts.value.reduce((points, amount, i) => {
         const x = (300 / total) * (i + 1);
         const y = amountToPixels(amount);
-        console.log(y);
         return `${points} ${x},${y}`;
     }, "0, 100");
 });
+
+const showPointer = ref(false);
+const pointer = ref(0);
+const tap = ({target,touches}) => {
+    showPointer.value = true;
+    const elementWidth = target.getBoundingClientRect().width;
+    const elementX = target.getBoundingClientRect().x;
+    const touchX = touches[0].clientX;
+
+    pointer.value = ((touchX - elementX) * 300) / elementWidth;
+}
+
+const unTap = (e) => {
+    showPointer.value = false;
+}
 </script>
 <style scoped>
 svg{
